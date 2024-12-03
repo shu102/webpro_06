d0ffbdd32c81fdeae0ba4eb0a1c16913a749b4d1
Aminaka shu <shured0825@gmail.com>
2024-12-03 10:52:56 +0900

    app6.jsを追加し，それに必要な.htmlや.jsを追加した

---
+++ public/test.js
@@ -0,0 +1,9 @@
+let btn1 = document.querySelector('#btn1');
+btn.addEventListener('click', () => {
+    console.log( "ボタン1が押されました" );
+});
+
+let btn2 = document.querySelector('#btn2');
+btn.addEventListener('click', () => {
+    console.log( "ボタン2が押されました" );
+});
